40a9ccde71e03d61240c113dfe6d2c5dd9659857
amikushkina <allamikushkina@gmail.com>
2013-11-03 14:32:08 +0200

    New changelist

--- src/Cassier.java
+++ src/Cassier.java
@@ -1,7 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class Cassier implements Runnable{
+
+    List<Meal> meals = new LinkedList<Meal>();
+
+    public Cassier(List<Meal> meals) {
+        this.meals = meals;
+
+    }
+
+
+
+
     @Override
     public void run() {
         //To change body of implemented methods use File | Settings | File Templates.
     }
+    public Meal giveMeal(){
+        return ;
+    }
+
+
 }
