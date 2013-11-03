49e014dc79a7a28d9daa883982cdc9b4de7627fc
amikushkina <allamikushkina@gmail.com>
2013-11-03 15:10:44 +0200

    qwert

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,16 @@
 
 public class Client extends Thread{
     int clientNum;
+    Cassier cassier;
 
-    public Client(int clientNum) {
+    public Client(int clientNum, Cassier cassier) {
         this.clientNum = clientNum;
-        Cassier cassier;
+        this.cassier= cassier;
     }
 
     @Override
     public void run() {
-
+        System.out.println("I've got" + cassier.giveMeal());
         super.run();    //To change body of overridden methods use File | Settings | File Templates.
     }
 
