477bf51d89b0218442730ef851761a6d31c09c91
amikushkina <allamikushkina@gmail.com>
2013-11-03 15:17:33 +0200

    qwert

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         Cassier cassier = new Cassier();
+
+
+        for(int i=0; i<=10; i++){
+            Client client = new Client(i,cassier);
+            client.start();
+        }
+
+
+
     }
 }
